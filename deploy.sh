df577ac403b7f9f576e27243f2a6fe6e31e6fa17
Nats <nocturno1470@gmail.com>
2019-07-01 20:53:51 -0500

    init before deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:nats-afs.github.io.git master
+# git push -f git@github.com:nats-afs/nats-afs.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:nats-afs/eventos-barzola.git master:gh-pages
 
 cd -
